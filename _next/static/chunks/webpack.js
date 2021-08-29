59118afaf8dc9b9a91d86ce9fdb48bd4125b42df
Hanson Wu <wuhonsun.hs@gmail.com>
2021-08-29 18:50:32 +0800

    add styles

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "837d1cc6972522fed73a"; }
+/******/ 		__webpack_require__.h = function() { return "76031a120396e23a5925"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
